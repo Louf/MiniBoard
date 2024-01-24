8fcc25d2fa195a6107275e2b9d0c874d3ddd7fde
Louis Farmer <louisfarmer@live.co.uk>
2024-01-24 15:40:49 -0800

    Initial Commit

---
+++ MiniBoard/MiniBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniBoardApp.swift
+//  MiniBoard
+//
+//  Created by Louis Farmer on 1/24/24.
+//
+
+import SwiftUI
+
+@main
+struct MiniBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
